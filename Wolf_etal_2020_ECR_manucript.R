ba586c72616d2fbc0bdca48092dbc0f3ee979efb
geneticsjesse <40606030+geneticsjesse@users.noreply.github.com>
2021-03-02 10:03:39 -0500

    Update Wolf_etal_2020_ECR_manucript.R
    update plot and model code

--- Wolf_etal_2020_ECR_manucript.R
+++ Wolf_etal_2020_ECR_manucript.R
@@ -15,21 +15,12 @@ dat<- read.csv("output/Dataset_Final.csv")
 dat_reduce <- dat %>%
   select(c(Num_publications_first, Num_publications_last,
            Num_preprints_first, Num_preprints_last, Institution_size_first, 
-           gender_first, gender_last, ECR_status_first_simplified, ECR_status_last_simplified))
+           ECR_status_first_simplified, ECR_status_last_simplified))
 
 # Ensuring all relevant data columns are factors
 dat_reduce$Institution_size_first<- as.factor (dat_reduce$Institution_size_first)
-dat_reduce$gender_first<- as.factor (dat_reduce$gender_first)
 dat_reduce$ECR_status_first_simplified<-as.factor(dat_reduce$ECR_status_first_simplified)
 dat_reduce$ECR_status_last_simplified<- as.factor(dat_reduce$ECR_status_last_simplified)
-
-# T-test to check if there is a significant difference in preprints between genders of first and last authors
-t.test(dat_reduce$Num_preprints_first~dat_reduce$gender_first)
-t.test(dat_reduce$Num_preprints_last~dat_reduce$gender_last)
-
-# T-test to check if there is a significant difference in preprints between ECR status of first and last authors
-t.test(dat_reduce$Num_preprints_first~dat_reduce$ECR_status_first_simplified)
-t.test(dat_reduce$Num_preprints_last~dat_reduce$ECR_status_last_simplified)
 
 # GLM
 # Assume poission distribution if I leave response as # preprints and offset # of total publications as a predictor  
@@ -44,48 +35,7 @@ summary(model1)
 
 #Figure 1a
 mycol <- c("#56B4E9", "#D55E00")
-ggbox_1b <- ggplot(data=subset(dat_reduce,!is.na(gender_first)),
-                      aes(x= gender_first, y=Num_preprints_first,
-                          fill=gender_first))+
-  geom_violin(draw_quantiles = c(.25, .5, .75, .95), alpha=0.5, scale= "count")+
-  geom_boxplot(width=0.08, outlier.shape = NA, color = "gray40",fill = "white", 
-               coef =0)+
-  ylab ("Number of preprint articles") +
-  xlab ("Gender")+
-  scale_x_discrete(labels=c("Female","Male"))+
-  scale_fill_manual(values=mycol)+
-  theme_classic(
-    base_size = 15,
-    base_family ="Times",
-  )+
-  theme(legend.position = "none")+
-  annotate("text", label = "bold(A)", 
-           x=0.5, y = 20, parse = TRUE,
-           size = 7)
-
-#Figure 1b
-ggbox_1a <- ggplot(data=subset(dat_reduce,!is.na(gender_last)),
-                     aes(gender_last,Num_preprints_last,
-                         fill=gender_last))+
-  geom_violin(draw_quantiles = c(.25, .5, .75, .95), alpha=0.5, scale= "count")+
-  geom_boxplot(width=0.08, outlier.shape = NA, color = "gray40", fill = "white",
-               coef =0)+
-  ylab ("Number of preprint articles") +
-  xlab ("Gender")+
-  scale_x_discrete(labels=c("Female","Male"))+
-  scale_fill_manual(values=mycol)+
-  theme_classic(
-    base_size = 15,
-    base_family ="Times",
-  )+
-  theme(legend.position = "none")+
-  theme(legend.position = "none")+
-  annotate("text", label = "bold(B)", 
-           x=0.5, y = 44, parse = TRUE,
-           size = 7) 
-
-#Figure 1c
-ggbox_1c <- ggplot(data=subset(dat_reduce,!is.na(gender_first)),
+ggbox_1a <- ggplot(data=subset(dat_reduce,!is.na(gender_first)),
                    aes(x= ECR_status_first_simplified, y=Num_preprints_first,
                        fill=ECR_status_first_simplified))+
   geom_violin(draw_quantiles = c(.25, .5, .75, .95), alpha=0.5, scale= "count")+
@@ -100,12 +50,12 @@ ggbox_1c <- ggplot(data=subset(dat_reduce,!is.na(gender_first)),
     base_family ="Times",
   )+
   theme(legend.position = "none")+
-  annotate("text", label = "bold(C)", 
+  annotate("text", label = "bold(A)", 
            x=0.5, y = 20, parse = TRUE,
            size = 7)
 
-#Figure 1d
-ggbox_1d <- ggplot(data=subset(dat_reduce,!is.na(gender_last)),
+#Figure 1b
+ggbox_1b <- ggplot(data=subset(dat_reduce,!is.na(gender_last)),
                    aes(ECR_status_last_simplified,Num_preprints_last,
                        fill=ECR_status_last_simplified))+
   geom_violin(draw_quantiles = c(.25, .5, .75, .95), alpha=0.5, scale= "count")+
@@ -121,12 +71,11 @@ ggbox_1d <- ggplot(data=subset(dat_reduce,!is.na(gender_last)),
   )+
   theme(legend.position = "none")+
   theme(legend.position = "none")+
-  annotate("text", label = "bold(D)", 
+  annotate("text", label = "bold(B)", 
            x=0.5, y = 44, parse = TRUE,
            size = 7) 
 # Combine using patchwork
 combined_total <- ggbox_1a+
   ggbox_1b+
-  ggbox_1c+
-  ggbox_1d+
-  plot_layout(ncol = 2, nrow = 2)
+  plot_layout(ncol = 2, nrow = 1)
+combined_total
