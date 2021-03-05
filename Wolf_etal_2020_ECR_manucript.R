17590252e420e87100cb97a02681c4a421f1ff5a
geneticsjesse <40606030+geneticsjesse@users.noreply.github.com>
2021-03-05 10:50:38 -0500

    Update Wolf_etal_2020_ECR_manucript.R
    adding #1) anova/chi for table 1 and clean up

--- Wolf_etal_2020_ECR_manucript.R
+++ Wolf_etal_2020_ECR_manucript.R
@@ -22,9 +22,53 @@ dat_reduce$Institution_size_first<- as.factor (dat_reduce$Institution_size_first
 dat_reduce$ECR_status_first_simplified<-as.factor(dat_reduce$ECR_status_first_simplified)
 dat_reduce$ECR_status_last_simplified<- as.factor(dat_reduce$ECR_status_last_simplified)
 
-# GLM
-# Assume poission distribution if I leave response as # preprints and offset # of total publications as a predictor  
-# Quasipoisson as poisson is over dispersed
+# 1) Create a series of models, building up from null model to full model by adding one term at a time.
+
+# null model
+model_null<- glm(Num_preprints_first ~ 1 +
+                   offset(log(Num_publications_first)),
+                data=dat_reduce,
+                family = quasipoisson)
+
+# add first author ECR status together with offset, which is associated
+model_first<- glm(Num_preprints_first ~ ECR_status_first_simplified + 
+                offset(log(Num_publications_first)),
+                data=dat_reduce,
+                family = quasipoisson)
+
+# add last author ECR status
+model_firstlast<- glm(Num_preprints_first ~ ECR_status_first_simplified + 
+                offset(log(Num_publications_first))+
+                ECR_status_last_simplified,
+                data=dat_reduce,
+                family = quasipoisson)
+
+# add institution size
+model_firstlastsize<- glm(Num_preprints_first ~ ECR_status_first_simplified + 
+                        offset(log(Num_publications_first))+
+                        ECR_status_last_simplified+
+                        Institution_size_first,
+                        data=dat_reduce,
+                        family = quasipoisson)
+
+# add the interaction 
+model_firstlastsizeint<- glm(Num_preprints_first ~ ECR_status_first_simplified + 
+                          offset(log(Num_publications_first))+
+                          ECR_status_last_simplified+
+                          Institution_size_first+
+                          ECR_status_first_simplified:Institution_size_first,
+                          data=dat_reduce,
+                          family = quasipoisson)
+
+# 2) Compare models stepwise to see if adding each term made the model significantly better than the previous simpler model.
+
+anova(model_null, model_first, test="Chi") # does adding first author status significantly improve the model?
+anova(model_first, model_firstlast, test="Chi") # does adding last author status significantly improve the model?
+anova(model_firstlast, model_firstlastsize, test="Chi") # does adding institution size significantly improve the model?
+anova(model_firstlastsize, model_firstlastsizeint, test="Chi") # does adding the interaction significantly improve the model?
+
+# 3) Generalized Linear Model 
+
 model1<- glm(Num_preprints_first ~ ECR_status_first_simplified + 
            ECR_status_last_simplified+
            offset(log(Num_publications_first))+
@@ -34,9 +78,11 @@ model1<- glm(Num_preprints_first ~ ECR_status_first_simplified +
          family = quasipoisson)
 summary(model1)
 
+# 4) Figure 1
+
 #Figure 1a
 mycol <- c("#56B4E9", "#D55E00")
-ggbox_1a <- ggplot(data=subset(dat_reduce,!is.na(gender_first)),
+ggbox_1a <- ggplot(data=dat_reduce,
                    aes(x= ECR_status_first_simplified, y=Num_preprints_first,
                        fill=ECR_status_first_simplified))+
   geom_violin(draw_quantiles = c(.25, .5, .75, .95), alpha=0.5, scale= "count")+
@@ -56,7 +102,7 @@ ggbox_1a <- ggplot(data=subset(dat_reduce,!is.na(gender_first)),
            size = 7)
 
 #Figure 1b
-ggbox_1b <- ggplot(data=subset(dat_reduce,!is.na(gender_last)),
+ggbox_1b <- ggplot(data=dat_reduce,
                    aes(ECR_status_last_simplified,Num_preprints_last,
                        fill=ECR_status_last_simplified))+
   geom_violin(draw_quantiles = c(.25, .5, .75, .95), alpha=0.5, scale= "count")+
@@ -75,11 +121,10 @@ ggbox_1b <- ggplot(data=subset(dat_reduce,!is.na(gender_last)),
   annotate("text", label = "bold(B)", 
            x=0.5, y = 44, parse = TRUE,
            size = 7) 
-#figure 1C
+#Figure 1C
 interact <- plot_model(hx, type = "int",
                        title="",
                        axis.title = c("", "Number of preprint articles"),
-                       #   axis.labels = c("ECR_status_first_simplified ="),
                        legend.title = "")+
   theme_classic(
     base_size = 15,
@@ -91,6 +136,5 @@ interact <- plot_model(hx, type = "int",
 # Combine using patchwork
 combined_total <- ggbox_1a+
   ggbox_1b+
-interact
+interact+
   plot_layout(ncol = 3)
-combined_total
