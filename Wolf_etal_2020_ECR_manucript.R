d4d343a2cafb3fdfef6b0be4f9f75d6844c3ebb5
geneticsjesse <40606030+geneticsjesse@users.noreply.github.com>
2021-03-28 13:18:36 -0400

    Update Wolf_etal_2020_ECR_manucript.R
    fixed plots and cleaned up

--- Wolf_etal_2020_ECR_manucript.R
+++ Wolf_etal_2020_ECR_manucript.R
@@ -3,8 +3,7 @@
 # Input: Final dataset in .csv format
 
 # Environment setup
-pkgs<- c('tidyverse','ggplot2', 'extrafont', 'sjPlot', 'patchwork',
-         'colorBlindness', 'hrbrthemes')
+pkgs<- c('tidyverse','ggplot2', 'extrafont', 'sjPlot', 'patchwork')
 invisible(lapply(pkgs, require, character=TRUE))
 dat<- read.csv("output/Dataset_Final.csv")
 
@@ -75,65 +74,21 @@ model1<- glm(Num_preprints_first ~ ECR_status_first_simplified +
          family = quasipoisson)
 summary(model1)
 
-# Are fixed effects (including interaction term) significant?
-# Step 1: Create models.
-# Step 2: Compare them.
+# Add predicted data to data frame and interaction plot column to group for plots
+dat_reduce$fit <- predict(hx)
+dat_reduce$interact <- paste(dat_reduce$Institution_size_first, "x", dat_reduce$ECR_status_first_simplified)
+dat_reduce$interact<- as.factor (dat_reduce$interact)
+str(dat_reduce)
 
-# 4) Create a series of models, building up from null model to full model by adding one term at a time.
-
-# null model
-model_null<- glm(Num_preprints_first ~ 1 +
-                   offset(log(Num_publications_first)),
-                data=dat_reduce,
-                family = quasipoisson)
-
-# add first author ECR status together with offset, which is associated
-model_first<- glm(Num_preprints_first ~ ECR_status_first_simplified + 
-                offset(log(Num_publications_first)),
-                data=dat_reduce,
-                family = quasipoisson)
-
-# add last author ECR status
-model_firstlast<- glm(Num_preprints_first ~ ECR_status_first_simplified + 
-                offset(log(Num_publications_first))+
-                ECR_status_last_simplified,
-                data=dat_reduce,
-                family = quasipoisson)
-
-# add institution size
-model_firstlastsize<- glm(Num_preprints_first ~ ECR_status_first_simplified + 
-                        offset(log(Num_publications_first))+
-                        ECR_status_last_simplified+
-                        Institution_size_first,
-                        data=dat_reduce,
-                        family = quasipoisson)
-
-# add the interaction - this is exactly the same as the "model_new"
-model_firstlastsizeint<- glm(Num_preprints_first ~ ECR_status_first_simplified + 
-                          offset(log(Num_publications_first))+
-                          ECR_status_last_simplified+
-                          Institution_size_first+
-                          ECR_status_first_simplified:Institution_size_first,
-                          data=dat_reduce,
-                          family = quasipoisson)
-
-# 5) Compare models stepwise to see if adding each term made the model significantly better than the previous simpler model.
-
-anova(model_null, model_first, test="Chi") # does adding first author status significantly improve the model?
-anova(model_first, model_firstlast, test="Chi") # does adding last author status significantly improve the model?
-anova(model_firstlast, model_firstlastsize, test="Chi") # does adding institution size significantly improve the model?
-anova(model_firstlastsize, model_firstlastsizeint, test="Chi") # does adding the interaction significantly improve the model?
-
-
-# 6) Figures
+# 4) Figures
 
 #Figure 1a
-mycol <- c("#56B4E9", "#D55E00")
+mycol<- c("#56B4E9", "#D55E00")
 ggbox_1a <- ggplot(data=dat_reduce,
-                   aes(x= ECR_status_first_simplified, y=Num_preprints_first,
+                   aes(x= ECR_status_first_simplified, y=fit,
                        fill=ECR_status_first_simplified))+
-  geom_violin(draw_quantiles = c(.25, .5, .75, .95), alpha=0.5, scale= "count")+
-  geom_boxplot(width=0.08, outlier.shape = NA, color = "gray40",fill = "white", 
+  geom_violin(draw_quantiles = c(.25, .5, .75, .95), alpha=0.5)+#, scale = "count")+
+  geom_boxplot(width=0.4, outlier.shape = NA, color = "black",fill = "white", 
                coef =0)+
   ylab ("Number of preprint articles") +
   xlab("")+
@@ -145,15 +100,15 @@ ggbox_1a <- ggplot(data=dat_reduce,
   )+
   theme(legend.position = "none")+
   annotate("text", label = "bold(A)", 
-           x=0.5, y = 20, parse = TRUE,
+           x=0.5, y = 5, parse = TRUE,
            size = 7)
-
+ggbox_1a
 #Figure 1b
 ggbox_1b <- ggplot(data=dat_reduce,
-                   aes(ECR_status_last_simplified,Num_preprints_last,
+                   aes(ECR_status_last_simplified,fit,
                        fill=ECR_status_last_simplified))+
-  geom_violin(draw_quantiles = c(.25, .5, .75, .95), alpha=0.5, scale= "count")+
-  geom_boxplot(width=0.062, outlier.shape = NA, color = "gray40", fill = "white",
+  geom_violin(draw_quantiles = c(.25, .5, .75, .95), alpha=0.5)+#, scale ="count")+
+  geom_boxplot(width=0.4, outlier.shape = NA, color = "black", fill = "white",
                coef =0)+
   ylab ("Number of preprint articles") +
   xlab ("")+
@@ -166,22 +121,43 @@ ggbox_1b <- ggplot(data=dat_reduce,
   theme(legend.position = "none")+
   theme(legend.position = "none")+
   annotate("text", label = "bold(B)", 
-           x=0.5, y = 44, parse = TRUE,
+           x=0.5, y = 5, parse = TRUE,
            size = 7) 
-#Figure 1C
-interact <- plot_model(hx, type = "int",
-                       title="",
-                       axis.title = c("", "Number of preprint articles"),
-                       legend.title = "")+
+ggbox_1b
+#figure 1c
+ggbox_1c <- ggplot(data=dat_reduce,
+                   aes(x=interact,y=fit,
+                       fill=ECR_status_first_simplified,
+                       group = interact))+
+  geom_violin(draw_quantiles = c(.25, .5, .75, .95), alpha=0.5)+
+  geom_boxplot(width= 0.4, outlier.shape = NA, color = "black", fill = "white",
+               coef =0)+
+  ylab ("Number of preprint articles") +
+  xlab ("")+
+  scale_fill_manual(values=mycol)+
   theme_classic(
     base_size = 15,
     base_family ="Times",
   )+
+  theme(legend.title = element_blank(),
+        legend.position = "none")+
   annotate("text", label = "bold(C)", 
-           x=0.5, y = 7.2, parse = TRUE,
-          size = 7) 
+           x=.6, y = 5, parse = TRUE,
+           size = 7) 
+ggbox_1c
+
 # Combine using patchwork
-combined_total <- ggbox_1a+
-  ggbox_1b+
-interact+
-  plot_layout(ncol = 3)
+design <- c(
+  area (1,1,2),
+  area (1,2,2),
+  area(3,1,4,2)
+)
+combined_total <- ggbox_1a+ ggbox_1b+  ggbox_1c+
+  plot_layout(design=design)
+
+ggsave("graphics/Fig1_final.tiff", 
+       dpi = 400, plot = combined_total,
+       width = 13, 
+       height = 8, 
+       unit = "in",
+       compression ="lzw")
